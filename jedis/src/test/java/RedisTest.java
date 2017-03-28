c9ab4927d22265e7608a581ea7cc8c55d9f01b7e
ieayoio <ieayoio@foxmail.com>
2017-03-28 15:43:59 +0800

    Jedis test

--- jedis/src/test/java/RedisTest.java
+++ jedis/src/test/java/RedisTest.java
@@ -2,13 +2,14 @@ import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Transaction;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by ieayoio on 2017/3/6.
+ *
+ * 参考：http://www.tianshouzhi.com/api/tutorials/redis/210
  */
 public class RedisTest {
 
@@ -42,12 +43,13 @@ public class RedisTest {
         //连接本地的 Redis 服务
         Jedis jedis = new Jedis("localhost");
         System.out.println("Connection to server sucessfully");
+        jedis.del("tutorial-list");
         //存储数据到列表中
         jedis.lpush("tutorial-list", "Redisss");
         jedis.lpush("tutorial-list", "Mongodb");
         jedis.lpush("tutorial-list", "Mysql");
         // 获取存储的数据并输出
-        List<String> list = jedis.lrange("tutorial-list", 0, 5);
+        List<String> list = jedis.lrange("tutorial-list", 0, 1000);
         for (int i = 0; i < list.size(); i++) {
             System.out.println("Stored string in redis:: " + list.get(i));
         }
@@ -70,6 +72,9 @@ public class RedisTest {
         }
     }
 
+    /**
+     * 连接池
+     */
     @Test
     public void JedisPoolConfigTest() {
 
@@ -85,5 +90,132 @@ public class RedisTest {
         pool.close();
     }
 
+
+//    /**
+//     * 高可用连接
+//     */
+//    @Test
+//    public void JedisSentinelPoolTest() {
+//
+//        Set<String> sentinels = new HashSet<String>();
+//        sentinels.add("127.0.0.1:6379");
+////        sentinels.add("172.18.18.208:26379");
+//        JedisPoolConfig config = new JedisPoolConfig();
+//        config.setMaxIdle(5);
+//        config.setMaxTotal(20);
+//        JedisSentinelPool pool = new JedisSentinelPool("mymaster", sentinels, config);
+//        Jedis jedis = pool.getResource();
+//        jedis.set("jedis", "jedis");
+//
+//        String s = jedis.get("jedis");
+//        System.out.println(s);
+//
+//
+//        jedis.close();
+//        pool.close();
+//    }
+
+
+    /**
+     * 连接池
+     */
+    @Test
+    public void HashTest() {
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(8);
+        config.setMaxTotal(18);
+        JedisPool pool = new JedisPool(config, "127.0.0.1", 6379, 2000, null);
+        Jedis jedis = pool.getResource();
+        String key = "user";
+        String field_name = "name";
+        String field_name_value = "tianshouzhi";
+        String field_city = "city";
+        String field_city_value = "shanghai";
+        jedis.hset(key, field_name, field_name_value);
+        jedis.hset(key, field_city, field_city_value);
+
+
+        Map<String, String> map = jedis.hgetAll(key);
+        Set<Map.Entry<String, String>> entrySet = map.entrySet();
+        for (Map.Entry<String, String> entry : entrySet) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        jedis.close();
+        pool.close();
+    }
+
+    //Redis和Java的集合实例
+    @Test
+    public void testSet() {
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(8);
+        config.setMaxTotal(18);
+        JedisPool pool = new JedisPool(config, "127.0.0.1", 6379, 2000, null);
+        Jedis jedis = pool.getResource();
+
+        String key = "set_key";
+        String[] members = new String[]{"a", "b", "a", "c"};
+        jedis.sadd(key, members);
+
+        Set<String> smembers = jedis.smembers(key);
+        for (String string : smembers) {
+            System.out.println(string);
+        }
+        jedis.close();
+        pool.close();
+
+    }
+
+
+    // Redis和Java的集合实例
+    @Test
+    public void testZSet() {
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(8);
+        config.setMaxTotal(18);
+        JedisPool pool = new JedisPool(config, "127.0.0.1", 6379, 2000, null);
+        Jedis jedis = pool.getResource();
+
+
+        String key = "zset_key";
+        Map<String, Double> scoreMembers = new HashMap<String, Double>() {{
+            put("tianshouzhi", 1.0);
+            put("huhuamin", 3.0);
+        }};
+        jedis.zadd(key, scoreMembers);
+
+
+        Set<String> zrange = jedis.zrange(key, 0, 2);
+
+        for (String member : zrange) {
+            System.out.println(member);
+            System.out.println(jedis.zscore(key, member));
+        }
+    }
+
+    @Test
+    public void transactionTest() {
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(8);
+        config.setMaxTotal(18);
+        JedisPool pool = new JedisPool(config, "127.0.0.1", 6379, 2000, null);
+        Jedis jedis = pool.getResource();
+
+        Transaction t = jedis.multi();
+
+        t.set("foo", "bar");
+        t.set("foo2", "bar");
+        t.set("foo3", "bar");
+        t.exec();
+
+        jedis.close();
+        pool.close();
+
+    }
 
 }
