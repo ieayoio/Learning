164963cb091ccf256793d7a6c70ada01c6282b4c
ieayoio <ieayoio@foxmail.com>
2017-06-09 16:26:37 +0800

    netty入门

---
+++ netty/src/main/java/cn/ieayoio/netty/DiscardServerHandler.java
@@ -0,0 +1,31 @@
+package cn.ieayoio.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Created by ieayoio on 2017/6/9.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+        // 或者直接打印
+        System.out.println("Yes, A new client in = " + ctx.name());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // 出现异常时关闭连接。
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
